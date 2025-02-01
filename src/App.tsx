078fe6ef801008e55c50b1b1e924f9f25a806b05
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2025-02-01 10:19:24 -0300

    style: add fade in with framer motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import Header from "./components/header";
 import { Product } from "./components/product";
-import { ProductCarouselDesktop } from "./components/product-carousel-desktop";
+import { ProductSlider } from "./components/product-slider";
 import { ProductProvider } from "./contexts/product";
 import { CarouselProvider } from "./contexts/carousel";
+import { motion } from "framer-motion";
 
 const App = () => {
   return (
-    <>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 2 }}
+    >
       <ProductProvider>
         <Header />
         <CarouselProvider>
           <Product />
-          <ProductCarouselDesktop />
+          <ProductSlider />
         </CarouselProvider>
       </ProductProvider>
-    </>
+    </motion.div>
   );
 };
 
